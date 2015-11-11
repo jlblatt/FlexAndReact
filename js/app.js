5ef10bc765fe8ce824fe1c64e0b6cf0d00059307
Jason Blatt <jason@jasonblatt.net>
2015-11-11 00:30:54 -0500

    First container control

--- js/app.js
+++ js/app.js
@@ -1,23 +1,113 @@
 var App = React.createClass({displayName: "App",
 
+  getDefaultProps: function() {
+    return {
+
+    };
+  }, //default props
+
+  getInitialState: function() {
+    return {
+      container_css : {
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        alignContent: "stretch"
+      }
+    };
+  }, //initial state
+
+  setAppState: function(newState) {
+    this.setState(newState);
+  }, //setAppState
+
   render: function() {
     return(
       React.createElement("div", {id: "main"}, 
         React.createElement("div", {className: "col sm"}, 
-          React.createElement("div", {id: "controls"}
-
-          )
+          React.createElement(Controls, {setAppState: this.setAppState})
         ), 
         React.createElement("div", {className: "col lg"}, 
-          React.createElement("div", {id: "canvas"}
-
-          )
+          React.createElement(Container, {container_css: this.state.container_css})
         )
       )
     );
   }
 
-}); //class app
+}); //class App
+
+
+
+var Controls = React.createClass({displayName: "Controls",
+
+  render: function() {
+    return(
+      React.createElement("div", {id: "controls"}, 
+        React.createElement("div", {className: "header"}, 
+          React.createElement("h1", null, "FlexAndReact"), 
+          React.createElement("h6", null, "FlexBox Builder Using React.js"), 
+          React.createElement("p", null, React.createElement("a", {target: "_blank", href: "https://github.com/jlblatt/FlexAndReact"}, "View On Github"))
+        ), 
+        React.createElement("div", {className: "panel container-settings"}, 
+          React.createElement(ContainerControls, {setAppState: this.props.setAppState})
+        )
+      )
+    )
+  }
+
+}); //class controls
+
+
+
+var ContainerControls = React.createClass({displayName: "ContainerControls",
+
+  updateContainer: function(e) {
+    var obj = {};
+    obj[$(e.target).data('css')] = $(e.target).val();
+    this.props.setAppState({ container_css : obj });
+  },
+
+  render: function() {
+    return(
+      React.createElement("div", {className: "control select"}, 
+        this.context.text, 
+        React.createElement("label", null, "flex-direction:", 
+          React.createElement("select", {"data-css": "flexDirection", onChange: this.updateContainer}, 
+            React.createElement("option", {value: "row"}, "row"), 
+            React.createElement("option", {value: "row-reverse"}, "row-reverse"), 
+            React.createElement("option", {value: "column"}, "column"), 
+            React.createElement("option", {value: "column-reverse"}, "column-reverse")
+          )
+        )
+      )
+    )
+  }
+
+}); //class ContainerControls
+
+
+
+var Container = React.createClass({displayName: "Container",
+
+  render: function() {
+    var containerStyle = {
+      display: "flex",
+      flexDirection: this.props.container_css.flexDirection,
+      flexWrap: this.props.container_css.flexWrap,
+      justifyContent: this.props.container_css.justifyContent,
+      alignItems: this.props.container_css.alignItems,
+      alignContent: this.props.container_css.alignContent
+    };
+
+    return(
+      React.createElement("div", {id: "container", style: containerStyle}
+
+      )
+    )
+  }
+
+}); //class Container
 
 
 
--- js/app.jsx
+++ js/app.jsx
@@ -1,24 +1,113 @@
-
 var App = React.createClass({
+
+  getDefaultProps: function() {
+    return {
+
+    };
+  }, //default props
+
+  getInitialState: function() {
+    return {
+      container_css : {
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        alignContent: "stretch"
+      }
+    };
+  }, //initial state
+
+  setAppState: function(newState) {
+    this.setState(newState);
+  }, //setAppState
 
   render: function() {
     return(
       <div id="main">
         <div className="col sm">
-          <div id="controls">
-
-          </div>
+          <Controls setAppState={this.setAppState} />
         </div>
         <div className="col lg">
-          <div id="canvas">
-
-          </div>
+          <Container container_css={this.state.container_css} />
         </div>
       </div>
     );
   }
 
-}); //class app
+}); //class App
+
+
+
+var Controls = React.createClass({
+
+  render: function() {
+    return(
+      <div id="controls">
+        <div className="header">
+          <h1>FlexAndReact</h1>
+          <h6>FlexBox Builder Using React.js</h6>
+          <p><a target="_blank" href="https://github.com/jlblatt/FlexAndReact">View On Github</a></p>
+        </div>
+        <div className="panel container-settings">
+          <ContainerControls setAppState={this.props.setAppState} />
+        </div>
+      </div>
+    )
+  }
+
+}); //class controls
+
+
+
+var ContainerControls = React.createClass({
+
+  updateContainer: function(e) {
+    var obj = {};
+    obj[$(e.target).data('css')] = $(e.target).val();
+    this.props.setAppState({ container_css : obj });
+  },
+
+  render: function() {
+    return(
+      <div className="control select">
+        {this.context.text}
+        <label>flex-direction:
+          <select data-css="flexDirection" onChange={this.updateContainer}>
+            <option value="row">row</option>
+            <option value="row-reverse">row-reverse</option>
+            <option value="column">column</option>
+            <option value="column-reverse">column-reverse</option>
+          </select>
+        </label>
+      </div>
+    )
+  }
+
+}); //class ContainerControls
+
+
+
+var Container = React.createClass({
+
+  render: function() {
+    var containerStyle = {
+      display: "flex",
+      flexDirection: this.props.container_css.flexDirection,
+      flexWrap: this.props.container_css.flexWrap,
+      justifyContent: this.props.container_css.justifyContent,
+      alignItems: this.props.container_css.alignItems,
+      alignContent: this.props.container_css.alignContent
+    };
+
+    return(
+      <div id="container" style={containerStyle}>
+
+      </div>
+    )
+  }
+
+}); //class Container
 
 
 
